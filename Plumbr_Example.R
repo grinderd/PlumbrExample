1e9144a4df2b60cfec351ee87604ee1b94b1526d
David Grinder <grinderd@gmail.com>
2018-02-17 01:20:43 -0500

    Added more complicated function

--- Plumbr_Example.R
+++ Plumbr_Example.R
@@ -10,3 +10,13 @@ normalMean <- function(samples=10){
 addTwo <- function(a, b){
   as.numeric(a) + as.numeric(b)
 }
+
+
+#* @get /meaninput
+#* @post /meaninput
+meaninput <- function (samples=100,m = 1){
+  data <- rnorm(samples,m)
+  mean(data)
+  var(data)
+  #return(mean(data),var(data))
+}
